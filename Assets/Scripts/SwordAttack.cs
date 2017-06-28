80d9a73cf1ea26e9d286e86eab2c6750214c54f1
KajG <kaaiman99@hotmail.com>
2017-06-28 10:19:58 +0200

    Removed Sword animation delay

--- Assets/Scripts/SwordAttack.cs
+++ Assets/Scripts/SwordAttack.cs
@@ -29,15 +29,12 @@ public class SwordAttack : MonoBehaviour {
 		}
 	}
 	void Attack(float damage){
-		StartCoroutine (Attacking(damage));
-	}
-	void AttackAnim(){
-		Destroy (Instantiate (anim, new Vector3(col.transform.position.x + col.offset.x * 2, col.transform.position.y, 0), Quaternion.identity), swordAnimTime);
-	}
-	IEnumerator Attacking(float damage){
-		yield return new WaitForSeconds (swordAnimTime);
 		for (int i = 0; i < checkenemy.enemyIndexes.Count; i++) {
 			checkenemy.enemies [checkenemy.enemyIndexes[i]].GetComponent<EnemyHealth> ().TakeDamage (damage);
 		}
 	}
+	void AttackAnim(){
+		Destroy (Instantiate (anim, new Vector3(col.transform.position.x + col.offset.x * 2, col.transform.position.y, 0), Quaternion.identity), swordAnimTime);
+	}
+
 }
