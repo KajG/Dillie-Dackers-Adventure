73222e41b78a4a3ba51404118dea8054ddeb4f48
KajG <kaaiman99@hotmail.com>
2017-06-28 10:17:13 +0200

    Healthbar toegevoegd en scripts georganiseerd

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour {
-
 	[SerializeField]private float health;
+	private Slider healthBar;
 	public float getHealth {get{return health;}set{health = value;}}
+	void Start(){
+		healthBar = GetComponentInChildren<Slider> ();
+		healthBar.maxValue = health;
+		healthBar.value = health;
+	}
 	public void TakeDamage(float damage){
 		if (damage < health) {
 			health -= damage;
 		} else {
 			Destroy (gameObject);
 		}
+		healthBar.value = health;
 	}
 }
